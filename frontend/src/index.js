960fd0ddf66676850b9da35b981606bb47c6ebee
kimjungminn24 <kimjungminn24@users.noreply.github.com>
2025-06-27 00:44:25 +0900

    feat : useQueries 활용해 데코 API 병렬 호출 및 캐싱 적용

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,13 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
+const queryClient = new QueryClient();
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <QueryClientProvider client={queryClient}>
     <App />
-  </React.StrictMode>
+  </QueryClientProvider>
 );
