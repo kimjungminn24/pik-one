c5673bf825ee1ed2b97a380bc7535b51b524b274
kimjungminn24 <kimjungminn24@users.noreply.github.com>
2025-06-28 15:16:12 +0900

    feat : 시큐리티 컨텍스트에서 회원 정보 확인 유틸 함수 추가

---
+++ backend/src/main/java/com/jeongmin/backend/utils/SecurityUtil.java
@@ -0,0 +1,19 @@
+package com.jeongmin.backend.utils;
+
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+@NoArgsConstructor(access = lombok.AccessLevel.PRIVATE)
+public class SecurityUtil {
+
+    public static String getCurrentProviderId() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        if (authentication == null || authentication.getPrincipal() == null) {
+            throw new IllegalStateException("현재 로그인 상태가 아닙니다.");
+        }
+
+        return authentication.getPrincipal().toString();
+    }
+}
