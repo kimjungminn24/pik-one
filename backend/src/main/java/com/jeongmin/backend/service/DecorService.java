4d65daf1c9947e1f4f732700bdea968a900d2f96
kimjungminn24 <kimjungminn24@users.noreply.github.com>
2025-06-28 15:19:07 +0900

    feat : 데코 모종 생성 API 구현

--- backend/src/main/java/com/jeongmin/backend/service/DecorService.java
+++ backend/src/main/java/com/jeongmin/backend/service/DecorService.java
@@ -1,10 +1,17 @@
 package com.jeongmin.backend.service;
 
+import com.jeongmin.backend.dto.DecorCreateRequest;
 import com.jeongmin.backend.dto.DecorResponse;
 import com.jeongmin.backend.dto.DecorSearchRequest;
+import com.jeongmin.backend.entity.Decor;
+import com.jeongmin.backend.entity.DecorType;
+import com.jeongmin.backend.entity.User;
 import com.jeongmin.backend.repository.DecorRepository;
+import com.jeongmin.backend.repository.UserRepository;
+import com.jeongmin.backend.utils.SecurityUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -14,6 +21,7 @@ public class DecorService {
 
 
     private final DecorRepository decorRepository;
+    private final UserRepository userRepository;
 
     public List<DecorResponse> searchDecorInBoundary(DecorSearchRequest request) {
         return decorRepository.findByBoundaryAndType(
@@ -26,6 +34,28 @@ public class DecorService {
                 .stream()
                 .map(DecorResponse::from)
                 .toList();
+    }
+
+    @Transactional
+    public DecorResponse createNewDecor(DecorCreateRequest request) {
+
+        String providerId = SecurityUtil.getCurrentProviderId();
+
+        User user = userRepository.findByProviderAndProviderId("naver", providerId)
+                .orElseThrow(() -> new IllegalArgumentException("유저를 찾을 수 없습니다."));
+
+        Decor decor = Decor.createNewDecor(
+                request.lat(),
+                request.lng(),
+                request.type(),
+                request.content(),
+                user
+        );
+
+        Decor saved = decorRepository.save(decor);
+
+        return DecorResponse.from(saved);
+
     }
 
 }
