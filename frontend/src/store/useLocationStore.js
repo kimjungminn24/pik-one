fb34586b8745eb71a3c9fddfef5bf48eae4cdce5
kimjungminn24 <kimjungminn24@users.noreply.github.com>
2025-07-04 00:55:25 +0900

    fix : 첫 검색 시 API 호출이 안되는 문제 해결

--- frontend/src/store/useLocationStore.js
+++ frontend/src/store/useLocationStore.js
@@ -6,10 +6,10 @@ export const useLocationStore = create((set) => ({
   setLocation: (lat, lng) => set({ lat, lng }),
 
   bounds: {
-    northLat: "",
-    southLat: "",
-    eastLng: "",
-    westLng: "",
+    northLat: 37.58,
+    southLat: 37.55,
+    eastLng: 126.99,
+    westLng: 126.97,
   },
   setBounds: (bounds) => set({ bounds }),
 }));
