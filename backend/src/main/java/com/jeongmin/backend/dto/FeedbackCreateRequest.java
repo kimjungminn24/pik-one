c3e5b1cb927d55c4dbe91d330607320301217712
kimjungminn24 <kimjungminn24@users.noreply.github.com>
2025-07-03 17:56:40 +0900

    fix : 피드백 등록시 프론트에서 유저 아이디 보낼 필요없으므로 삭제

--- backend/src/main/java/com/jeongmin/backend/dto/FeedbackCreateRequest.java
+++ backend/src/main/java/com/jeongmin/backend/dto/FeedbackCreateRequest.java
@@ -3,7 +3,6 @@ package com.jeongmin.backend.dto;
 import com.jeongmin.backend.entity.FeedbackType;
 
 public record FeedbackCreateRequest(
-        Long userId,
         Long decorId,
         FeedbackType feedbackType,
         String content
