496adde6f894a57f0abff2fde1284a1481abcf46
kimjungminn24 <kimjungminn24@users.noreply.github.com>
2025-07-03 17:51:36 +0900

    feat : 팝업 컴포넌트 구현 및 맵 컴포넌트에 적용

--- frontend/src/components/map/MapComponent.jsx
+++ frontend/src/components/map/MapComponent.jsx
@@ -1,41 +1,55 @@
-import React from "react";
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import React, { useState } from "react";
+import { MapContainer, TileLayer, Marker } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import { LocateButton } from "./LocateButton";
 import { LocationMarker } from "./LocationMarker";
-import { customIcon } from "../../utils/markerIcon";
 import { FetchOnMove } from "./FetchOnMove";
+import { useDecorDetail } from "../../hooks/useDecor";
+import DecorDetailPopup from "../decor/DecorDetailPopup";
 import { createCustomIcon } from "../../utils/markerIcon";
 
 export default function MapComponent({
   searchResults = [],
   showLocationMarker = true,
 }) {
-  return (
-    <MapContainer
-      center={[37.5665, 126.978]}
-      zoom={13}
-      className="leaflet-container"
-    >
-      <TileLayer
-        attribution="&copy; OpenStreetMap contributors"
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
+  const [selectedId, setSelectedId] = useState(null);
+  const { data, isLoading, isError } = useDecorDetail(selectedId, !!selectedId);
 
-      {searchResults.map((item) => (
-        <Marker
-          key={`${item.id}-${item.type}`}
-          position={[item.lat, item.lng]}
+  return (
+    <>
+      <MapContainer
+        center={[37.5665, 126.978]}
+        zoom={13}
+        className="leaflet-container"
+      >
+        <TileLayer
+          attribution="&copy; OpenStreetMap contributors"
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+
+        {searchResults.map((item) => (
+          <Marker
+            key={`${item.id}-${item.type}`}
+            position={[item.lat, item.lng]}
             icon={createCustomIcon(item.type, item.id === selectedId)}
             eventHandlers={{
               click: () => setSelectedId(item.id),
             }}
           />
-      ))}
+        ))}
 
         {showLocationMarker && <LocationMarker />}
-      <LocateButton />
-      <FetchOnMove />
-    </MapContainer>
+        <LocateButton />
+        <FetchOnMove />
+      </MapContainer>
+      {selectedId && data && !isLoading && !isError && (
+        <div className="mobile-popup-panel">
+          <button className="close-button" onClick={() => setSelectedId(null)}>
+            ✕
+          </button>
+          <DecorDetailPopup data={data} />
+        </div>
+      )}
+    </>
   );
 }
