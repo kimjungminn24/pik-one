7feb64408ee927d2672ef010eedca332b6d558cf
kimjungminn24 <kimjungminn24@users.noreply.github.com>
2025-07-09 19:57:55 +0900

    feat : 로그인 여부에 따라 로그아웃 버튼 추가

--- frontend/src/components/HeaderComponent.jsx
+++ frontend/src/components/HeaderComponent.jsx
@@ -1,12 +1,14 @@
 import { useState, useEffect } from "react";
 import { Link, useLocation } from "react-router-dom";
 import { useUserStore } from "../store/useUserStore";
+import { useLogoutMutation } from "../hooks/useUser";
 
 export default function Header() {
   const [isOpen, setIsOpen] = useState(false);
   const location = useLocation();
+  const { mutate: logout } = useLogoutMutation();
 
-  const { isLogin } = useUserStore();
+  const isLogin = useUserStore((state) => state.isLogin);
 
   useEffect(() => {
     setIsOpen(false);
@@ -28,10 +30,25 @@ export default function Header() {
         </div>
 
         <nav className="nav">
-          <Link to="/register">모종 등록</Link>
-          <Link to="/find">모종 찾기</Link>
+          <Link to="/register" className="nav-item">
+            모종 등록
+          </Link>
+          <Link to="/find" className="nav-item">
+            모종 찾기
+          </Link>
           {isLogin ? (
-            <Link to="/mypage"> 내 정보</Link>
+            <>
+              <Link to="/mypage" className="nav-item">
+                내 정보
+              </Link>
+
+              <button
+                onClick={() => logout()}
+                className="nav-item logout-button"
+              >
+                로그아웃
+              </button>
+            </>
           ) : (
             <button onClick={handleLogin} className="login-button-wrapper">
               <img src="/btn.png" alt="로그인버튼" className="login-button" />
@@ -39,23 +56,32 @@ export default function Header() {
           )}
         </nav>
 
-        <div className="menu-icon" onClick={toggleMenu}>
+        <div className="menu-icon " onClick={toggleMenu}>
           {isOpen ? "✖" : "☰"}
         </div>
       </div>
 
       <div className={`mobile-menu ${isOpen ? "show" : ""}`}>
-        <Link to="/register" onClick={toggleMenu}>
+        <Link to="/register" className="mobile-item" onClick={toggleMenu}>
           모종 등록
         </Link>
-        <Link to="/find" onClick={toggleMenu}>
+        <Link to="/find" className="mobile-item" onClick={toggleMenu}>
           모종 찾기
         </Link>
 
         {isLogin ? (
-          <Link to="/mypage" onClick={toggleMenu}>
-            내 정보
-          </Link>
+          <>
+            <Link to="/mypage" className="mobile-item">
+              내 정보
+            </Link>
+
+            <button
+              onClick={() => logout()}
+              className="logout-button mobile-item"
+            >
+              로그아웃
+            </button>
+          </>
         ) : (
           <button onClick={handleLogin} className="login-button-wrapper">
             <img src="/btn.png" alt="로그인버튼" className="login-button" />
