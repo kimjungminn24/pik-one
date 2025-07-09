cd7a728c2e117907712d535e2c5c3d803c350caa
kimjungminn24 <kimjungminn24@users.noreply.github.com>
2025-07-09 19:57:28 +0900

    feat : 로그아웃 API 연동 및 리액트 쿼리 적용

--- frontend/src/hooks/useUser.js
+++ frontend/src/hooks/useUser.js
@@ -1,7 +1,8 @@
-import { useQuery } from "@tanstack/react-query";
-import { fetchUser } from "../api/user";
+import { useQuery, useMutation } from "@tanstack/react-query";
+import { fetchUser, logout } from "../api/user";
 import { useUserStore } from "../store/useUserStore";
 import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 export const useUserQuery = () => {
   const setIsLogin = useUserStore((state) => state.setIsLogin);
@@ -23,4 +24,17 @@ export const useUserQuery = () => {
   }, [query.data, setIsLogin, logout]);
 
   return query;
+};
+
+export const useLogoutMutation = () => {
+  const logoutStore = useUserStore((state) => state.logout);
+  const navigate = useNavigate();
+
+  return useMutation({
+    mutationFn: logout,
+    onSuccess: () => {
+      logoutStore();
+      navigate("/");
+    },
+  });
 };
