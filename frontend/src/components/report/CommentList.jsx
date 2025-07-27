1ad5caebc2232ea58235636ad28b53dded54afd0
kimjungminn24 <kimjungminn24@users.noreply.github.com>
2025-07-27 19:22:18 +0900

    feat : 댓글 리스트 컴포넌트 추가

---
+++ frontend/src/components/report/CommentList.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+export default function CommentList({ list }) {
+  return (
+    <div className="comment-list">
+      {list.map((item, idx) => (
+        <div key={idx} className="comment-item">
+          <div className="comment-item-header">
+            <div
+              className="comment-item-emoji emoji"
+              style={{ backgroundColor: item.bgColor }}
+            >
+              {item.emoji}
+            </div>
+            <div className="comment-item-nickname">{item.nickname}</div>
+          </div>
+          <div className="comment-item-text">{item.comment}</div>
+          <div className="comment-item-meta">
+            {item.category} / {item.createdAt}
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+}
