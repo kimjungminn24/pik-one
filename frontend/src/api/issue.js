5016d13bcec6bd7990325ea0d4cbe0ae782aedb6
kimjungminn24 <kimjungminn24@users.noreply.github.com>
2025-08-01 17:00:21 +0900

    feat : 이슈 추가 및 이슈 리스트 api 연결

---
+++ frontend/src/api/issue.js
@@ -0,0 +1,26 @@
+import { handleFetchError } from "../utils/handleFetchError";
+const BASE_URL = `${process.env.REACT_APP_BACKEND_URL}/issue`;
+
+export const postIssue = async ({ body }) => {
+  const res = await fetch(`${BASE_URL}`, {
+    method: "POST",
+    credentials: "include",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ body }),
+  });
+
+  if (!res.ok) {
+    await handleFetchError(res);
+  }
+};
+
+export const getIssues = async () => {
+  const res = await fetch(`${BASE_URL}`);
+
+  if (!res.ok) {
+    await handleFetchError(res);
+  }
+  return res.json();
+};
