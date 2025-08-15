10eeff16de4f4f9c73b6c384765d0139fa180027
kimjungminn24 <kimjungminn24@users.noreply.github.com>
2025-08-15 15:59:40 +0900

    refactor : 사용 안하는 함수 제거

--- frontend/src/api/decor.js
+++ frontend/src/api/decor.js
@@ -81,43 +81,8 @@ export const fetchDecorDetail = async (id) => {
   return res.json();
 };
 
-export const createNewFeedback = async ({ decorId, feedbackType, content }) => {
-  const res = await fetch(`${BASE_URL}/feedback`, {
-    method: "POST",
-    credentials: "include",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      decorId,
-      feedbackType,
-      content,
-    }),
-  });
-
-  if (!res.ok) {
-    await handleFetchError(res);
-  }
-
-  const data = await res.json();
-  return data;
-};
-
 export const fetchMyDecors = async () => {
   const res = await fetch(`${BASE_URL}/me`, {
-    credentials: "include",
-  });
-
-  if (!res.ok) {
-    await handleFetchError(res);
-  }
-  const data = await res.json();
-
-  return data;
-};
-
-export const fetchMyFeedbacks = async () => {
-  const res = await fetch(`${BASE_URL}/me/feedbacks`, {
     credentials: "include",
   });
 
