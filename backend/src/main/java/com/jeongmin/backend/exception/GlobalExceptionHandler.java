21f508a4405d689fbbcd4301bad6ee25d69e03f9
kimjungminn24 <kimjungminn24@users.noreply.github.com>
2025-10-30 14:27:49 +0900

    feat: 에러 로그 추가

--- backend/src/main/java/com/jeongmin/backend/exception/GlobalExceptionHandler.java
+++ backend/src/main/java/com/jeongmin/backend/exception/GlobalExceptionHandler.java
@@ -1,5 +1,7 @@
 package com.jeongmin.backend.exception;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -8,13 +10,17 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 @RestControllerAdvice
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
+    private static final Logger log = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
     @ExceptionHandler(RestApiException.class)
     public ResponseEntity<ErrorResponse> handleRestApiException(RestApiException e) {
+        log.error("서버 내부 오류 발생: code={}, message={}", e.getErrorCode(), e.getMessage());
         return handleExceptionInternal(e.getErrorCode());
     }
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ErrorResponse> handleException(Exception e) {
+        log.error("[Unhandled Exception]", e);
         return handleExceptionInternal(ErrorCode.INTERNAL_ERROR);
     }
 
